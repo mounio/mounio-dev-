b3a1a91534de3c5e6293842f1746a0b165345562
mounio <59267862+mounio@users.noreply.github.com>
2023-12-30 20:15:50 +0100

    copy change

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,10 +5,23 @@ function Home() {
     <div className='home-about'>
         <h2>About me</h2>
         <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-            Sint tenetur veniam facilis, 
-            laborum deserunt corporis?
+          A few years ago I started learning c#, which quickly became one of my favorite programming languages.
+          I am quite self-taught and learn very quickly, I am social and like to share my knowledge and discoveries.
+          I like to stay up to date with new technologies, which is very important as a
+          developer.
         </p>
+        <div>
+        <h4>skils</h4>
+          <ul>
+            <li>.NET 6.0 - C#</li>
+            <li>ASP.NET en REST API’s (beginner)</li>
+            <li>Reactjs/React native (Expo)</li>
+            <li>MS SQL</li>
+            <li>HTML, CSS, JS/TypeScript</li>
+            <li>Github/Git (beginner)</li>
+          </ul>
+
+        </div>
 
         <nav>
             <NavLink to="Projects">Projects</NavLink>
